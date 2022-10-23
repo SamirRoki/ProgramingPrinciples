4625c59d65307da218f940e149016c13e8846277
Lejla Čarovac-Mehmedović <samir_mehmedovic@live.com>
2022-10-23 22:39:11 +0200

    try to commit pp

---
+++ swich/swich.js
@@ -0,0 +1,217 @@
+//1. Write a program that shows text representation of a day in a week for a number input from
+//1 to 7. Print output in console.
+//For input 1, output should be “Monday”.
+/*
+2. Write a program that shows text representation of a day in a week for a number input from
+1 to 7. All other cases output a message explaining that input must be a number between 1
+and 7.
+For input 1, output should be “Monday”.
+For input 10, output should be “Input must be a number between 1 and 7”.
+var weekDay = 2;
+var result = " ";
+switch (weekDay)
+{
+    case 1:
+     result =   "Monday";
+    break;
+case 2:
+   result = "Thusday";
+    break;
+ case 3:
+       result = "Wendsday";
+    break;
+case 4:
+       result = "Thusday";
+    break;
+case 5:
+            result ="Frydey";
+     break;
+ case 6:
+               result = "Saturday";
+    break;
+case 7:
+                    result ="Sundey";
+     break;
+                    default:
+                    result = "input must be number betwen 1 and 7";
+                    
+ 
+
+
+}
+console.log (result);*/
+
+
+
+/*3. Write a program that for a 1-7 number input (representing a day in a week) shows if that day
+is a weekday or weekend. All other cases output a message explaining that input must be a
+number between 1 and 7.
+For input 2, output should be “It’s weekday”.
+For input 6, output should be “It’s weekend”.
+For input 12, output should be “Input must be number between 1 and 7”.*/
+
+
+/*var weekDay = 2;
+var result = "";
+switch (weekDay){
+case 1: 
+case 2: 
+case 3: 
+case 4: 
+case 5: 
+result = "is week day";
+break
+case 6: 
+case 7: 
+result = "is weekand";
+default: 
+result = "input is not a number";}
+console.log (result);*/
+
+/*4. Write a program that for a 1-12 number input (representing a month in a year) shows that
+month’s name. All other cases output a message explaining that input must be a number
+between 1 and 12.
+For input 2, output should be “February”.
+For input 6, output should be “June”.
+For input 13, output should be “Input must be a number between 1 and 12”.*/
+
+/*var month = 5;
+var result = " ";
+switch (month) {
+case 1:
+        result =  "january" ;
+    break;
+ case 2:
+        result = "february" ;
+    break ;
+case 3:
+        result = "march" ;
+    break ;
+case 4:
+        result = "aprill" ;
+    break ;
+case 5:
+        result = "may" ;
+    break ;
+case 6:
+        result = "june" ;
+    break ;
+case 7:
+        result = "july" ;
+    break ;
+case 8:case 1:
+        result =  "january" ;
+    break;
+ case 2:
+        result = "february" ;
+    break ;
+case 3:
+        result = "march" ;
+    break ;
+case 4:
+        result = "aprill" ;
+    break ;
+case 5:
+        result = "may" ;
+    break ;
+case 6:
+        result = "june" ;
+    break ;
+case 7:
+        result 
+        result = "august" ;
+    break ;
+case 9:
+        result = "septembar" ;
+    break ;
+case 10:
+        result = "oktobar" ;
+    break ;
+case 11:
+        result = "novembar" ;
+    break;
+case 12:
+        result = "decembar" ;
+    default:
+        result = "must be a number between 1 and 12";
+        
+
+
+}
+console.log (result);*/
+/*5. Write a program that for a 1-12 number input (representing a month in a year) shows what
+season it is. All inputs different from 1-12 output a message explaining that input must be a
+number between 1 and 12.*/
+
+/*var season = 2 ;
+var result = " ";
+
+switch (season) {
+    case 12:
+    case 1:
+    case 2:
+    result = "winter";
+    break ;
+case 3:
+case 4:
+case 5:
+        result = "spring" ;
+    break ;
+case 6:
+case 7:
+case 8:
+        result =  "summer" ;
+    break;
+case 9:
+case 10:
+case 11:
+        result = "autumn" ;
+ defoult:
+ result = "input must be number between 1 and 12";
+}
+console.log (result);*/
+
+/*6. Write a program that for a string input of a grade from “A”-“F” outputs a proper info
+message about that grade in the following manner: A - &quot;Good job&quot;&quot;, B - &quot;Pretty good&quot;&quot;, C -
+&quot;Passed&quot;&quot;, D - &quot;Not so good&quot;&quot;, F - &quot;Failed&quot;. Input different from letters A-F outputs a message
+&quot;Unknown grade&quot;.*/
+
+/*var grade = "c";
+var result = " ";
+
+ switch (grade){
+case "a":
+    result = ";Good job";
+    break ;
+
+case "b":
+    result = "Pretty good";
+    break ;
+
+case "c":
+    result = "Passed";
+    break;
+
+case "d":
+    result = "Not so good";
+    break ;
+
+case "f":
+    result = " Failed";
+    break ;
+    default:
+        result = "unknown grade";
+    
+        
+
+
+ }
+ console.log (result);*/
+
+
+ 
+
+
+
+
+
